40f0328a91236a5e72f3a58ee1941da8be404d20
Suman Gurung <suman_201900250@smit.smu.edu.in>
2024-03-31 21:12:40 +0530

    add: added Personalize section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Feature from "@/components/screen/Feature";
 import Hero from "@/components/screen/Hero";
+import Personalize from "@/components/screen/Personalize";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <Feature />
+      <Personalize />
     </>
   );
 }
