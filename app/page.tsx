71667a30a2a55957f97f92b9ac37c1fe9eec529c
Suman Gurung <suman_201900250@smit.smu.edu.in>
2024-04-01 11:14:07 +0530

    add: added Solution component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Feature from "@/components/screen/Feature";
 import Hero from "@/components/screen/Hero";
 import Personalize from "@/components/screen/Personalize";
+import Solution from "@/components/screen/Solution";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Hero />
       <Feature />
       <Personalize />
+      <Solution />
     </>
   );
 }
