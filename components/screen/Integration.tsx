052cef69a0de2144621b9e442fdd16656aa13011
Suman Gurung <suman_201900250@smit.smu.edu.in>
2024-04-02 13:00:55 +0530

    fix: added image and gradient blob

--- components/screen/Integration.tsx
+++ components/screen/Integration.tsx
@@ -1,10 +1,13 @@
 import { CircleCheck } from "lucide-react";
 import React from "react";
 import { features } from "@/utlis/constant";
+import Image from "next/image";
+import Tools from "@/assets/integration-assets/integration.png";
+import Blob from "../ui/Blob";
 
 function Integration() {
   return (
-    <main id="integrations" className="center mt-2 flex-col pt-8">
+    <main id="integrations" className="center relative mt-2 flex-col pt-8">
       {/* Integration heading */}
       <h1 className="w-full max-w-xl text-center text-3xl font-bold md:text-4xl">
         Direct <span className="gradient-text">integrations</span> with your
@@ -35,6 +38,17 @@ function Integration() {
           Book a demo
         </button>
       </div>
+
+      <Image
+        src={Tools}
+        alt="integration image"
+        className="mt-20 w-full max-w-[600px]"
+      />
+
+      {/* floating gradient blobs */}
+      <Blob className="absolute left-[0px] top-[300px] z-[-10] hidden h-[500px] w-[500px] rounded-full bg-pink-500 opacity-10 blur-3xl lg:block" />
+
+      <Blob className="absolute right-[0px] top-[0px] z-[-10] hidden h-[500px] w-[500px] rounded-full bg-blue-500 opacity-10 blur-3xl lg:block" />
     </main>
   );
 }
